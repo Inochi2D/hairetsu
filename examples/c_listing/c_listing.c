42b26ee73723b23bc3ee8e1b51e9fb2bddb38916
LunaTheFoxgirl <luna@foxgirls.gay>
2025-07-28 03:48:33 +0200

    Improve example further.

--- examples/c_listing/c_listing.c
+++ examples/c_listing/c_listing.c
@@ -6,14 +6,17 @@
 void main() {
         ha_try_initialize();
 
-        // Enumerate families
+        // Get collection from all system fonts.
         ha_collection_t* c = ha_collection_create_from_system(true);
+
+        // Enumerate families
         uint32_t fcount = ha_collection_get_family_count(c);
         ha_family_t** families = ha_collection_get_families(c);
-
         for(size_t i = 0; i < fcount; i++) {
                 printf("Font %lu: %s\n", i, ha_family_get_name(families[i]));
         }
 
+        // Cleanup.
+        ha_release(c);
         ha_try_shutdown();
 }
