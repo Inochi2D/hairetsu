bf933eb6d3a0794f69c22c1810e6795977763af1
LunaTheFoxgirl <luna@foxgirls.gay>
2025-07-28 03:34:03 +0200

    Add (linux/posix) example of using hairetsu

---
+++ examples/c_listing/c_listing.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "../../out/hairetsu.h"
+
+void main() {
+        ha_try_initialize();
+
+        // Enumerate families
+        ha_collection_t* c = ha_collection_create_from_system(true);
+        uint32_t fcount = ha_collection_get_family_count(c);
+        ha_family_t** families = (ha_family_t**)ha_collection_get_families(c);
+
+        for(size_t i = 0; i < fcount; i++) {
+                printf("Font %lu: %s\n", i, ha_family_get_name(families[i]));
+        }
+
+        ha_try_shutdown();
+}
